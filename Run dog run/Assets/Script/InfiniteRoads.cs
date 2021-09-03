cc924f5f40755fdde609864cdb26a3b42e90b6c9
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2021-09-03 21:23:09 +1200

    Will generate new roads
    distance required 67.5 for no gaps

--- Run dog run/Assets/Script/InfiniteRoads.cs
+++ Run dog run/Assets/Script/InfiniteRoads.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class InfiniteRoads : MonoBehaviour
 {
     public GameObject[] roadPrefabs;
+    public float RoadWorks = 0;
+    public float Length = 84.5f;
+    public float NextRoad = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTile(0);
+        spawnTile(1);
+        spawnTile(2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void spawnTile(int i)
+    {
+        
+        Instantiate(roadPrefabs[i], new Vector3(50,0,NextRoad), Quaternion.Euler(0,90,0));
+        NextRoad += Length;
     }
 }
