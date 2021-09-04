d2cedbfec63331af18e007b0653461601c07fa97
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2021-09-05 00:25:46 +1200

    added gravity
    finished infiniteRoads

--- Run dog run/Assets/Script/InfiniteRoads.cs
+++ Run dog run/Assets/Script/InfiniteRoads.cs
@@ -6,25 +6,42 @@ public class InfiniteRoads : MonoBehaviour
 {
     public GameObject[] roadPrefabs;
     public float RoadWorks = 0;
-    public float Length = 84.5f;
+    public float Length = 67.5f;
     public float NextRoad = 0;
-    // Start is called before the first frame update
+    public int num = 2;
+    public Transform playT;
+    private int nextRoad = 65;
+    private List<GameObject> RoadPieces = new List<GameObject>();
     void Start()
     {
-        spawnTile(0);
-        spawnTile(1);
-        spawnTile(2);
+        for (int i = 0; i< num; i++)
+        {
+            spawnTile(Random.Range(0, roadPrefabs.Length));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playT.position.z>nextRoad)
+        {
+            spawnTile(Random.Range(0, roadPrefabs.Length));
+            nextRoad += 65;
+            RemoveLastRoadPiece();
+        }
     }
     public void spawnTile(int i)
     {
         
-        Instantiate(roadPrefabs[i], new Vector3(0,0,NextRoad), Quaternion.Euler(0,90,0));
+        GameObject obj = Instantiate(roadPrefabs[i], new Vector3(0,0,NextRoad), Quaternion.Euler(0,90,0));
+        RoadPieces.Add(obj);
         NextRoad += Length;
+    }
+
+    public void RemoveLastRoadPiece()
+    {
+        Destroy(RoadPieces[0]);
+        RoadPieces.RemoveAt(0);
+        
     }
 }
